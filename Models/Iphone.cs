425846c2db5e13f66530e0347a59e0656d25842a
Hebert <hebertsrocha78@gmail.com>
2024-01-28 17:50:02 -0300

    projeto finalizado

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero) : base(numero)
+        {
+            
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} no Iphone atraves do swift. ");
+        }
     }
 }
